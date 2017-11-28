bed77f541438a6ff74448e00737796ec97791e24
Jeremy <jmpetrot@buffalo.edu>
2017-11-28 12:55:43 -0500

    Initial commit

---
+++ common/header.php
@@ -0,0 +1,23 @@
+<header class="top-bar asee-header">
+    <div class="top-bar-left">
+        <ul class="dropdown medium-horizontal menu" data-dropdown-menu>
+            <li class="menu-text">ASEE 2017</li>
+            <li class="asee-menu-option"><a href="#">Dates</a></li>
+            <li class="asee-menu-option"><a href="#">Presentations</a></li>
+            <li class="asee-menu-option"><a href="#">Writing</a></li>
+            <li class="asee-menu-option">
+                <a href="#">Resources</a>
+                <ul class="menu vertical">
+                    <li class="asee-menu-option"><a href="#">Using Sources</a></li>
+                    <li class="asee-menu-option"><a href="#">Punctuation</a></li>
+                    <li class="asee-menu-option"><a href="#">Visuals</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <div class="top-bar-right hide-for-small-only">
+        <ul class="dropdown medium-horizontal menu" data-dropdown-menu>
+            <li><a class="success button" href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register&nbsp;</a></li>
+        </ul>
+    </div>
+</header>
